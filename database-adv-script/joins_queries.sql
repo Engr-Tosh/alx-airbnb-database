2cbf36285844127eb4537a1b86350f087c00a6bc
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-07-01 00:22:32 +0100

    Update script

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,11 +1,12 @@
 SELECT b.booking_id, u.name
 FROM "Booking" as b 
-INNER JOIN "User" AS u ON u.user_id = b.user_id
+INNER JOIN "User" AS u ON u.user_id = b.user_id;
 
 SELECT property.property_id, review.review_id
-FROM properties
+FROM property
 LEFT JOIN review ON property.property_id = review.property_id
+ORDER BY review.review_id DESC;
 
 SELECT user.name, booking.booking_id
 FROM user
-FULL OUTER JOIN booking on user.user_id = booking.user_id
+FULL OUTER JOIN booking on user.user_id = booking.user_id;
