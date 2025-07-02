6fdae3b2965a17e643c79632ee4e458311ec885c
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-07-02 01:45:12 +0100

    Fix aggregate sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,5 +4,6 @@ GROUP BY user_id;
 
 SELECT property_id, COUNT(*) AS total_bookings,
 ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS property_ranking
+ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS unique_row_number
 FROM booking
 GROUP BY property_id;
