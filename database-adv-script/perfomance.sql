1f5e5c86f1f2870b3b45f25b994ff5f590a67bef
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-07-02 05:24:44 +0100

    Rename performance to perfomance

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,11 @@
-SELECT * booking_id, user_id, property_id, payment_id
-FROM booking
-INNER JOIN payment on booking_id = booking.booking_id 
+ SELECT 
+  b.booking_id,
+  u.first_name,
+  u.last_name,
+  p.name AS property_name,
+  pay.amount,
+  pay.status
+FROM booking b, users u, property p, payment pay
+WHERE b.user_id = u.user_id
+  AND b.property_id = p.property_id
+  AND b.payment_id = pay.payment_id;
