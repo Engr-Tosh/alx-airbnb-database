0ee2f904e8b487ff3e9de47c9d6303d8f8343659
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-07-02 05:21:55 +0100

    Update perfomance and optimization report

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -6,7 +6,6 @@ SELECT
   pay.amount,
   pay.status
 FROM booking b, users u, property p, payment pay
-WHERE b.user_id = u.user_id
-  AND b.property_id = p.property_id
-  AND b.payment_id = pay.payment_id;
+WHERE b.user_id = u.user_id AND b.property_id = p.property_id
+AND b.payment_id = pay.payment_id;
  
