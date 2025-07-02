d5148582ce90ca7d9a1e13f74eed3f870324e68d
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-07-02 01:40:21 +0100

    Add aggregations and windows function sql file

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,8 @@
+SELECT user_id, COUNT(*) AS total_bookings,
+FROM booking
+GROUP BY user_id;
+
+SELECT property_id, COUNT(*) AS total_bookings,
+RANK() OVER (ORDER BY COUNT(*) DESC) AS property_ranking
+FROM booking
+GROUP BY property_id;
